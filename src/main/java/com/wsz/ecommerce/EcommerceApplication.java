752e8f877d3b9279043cef43e3860b22fb00d8ab
Urie <urie.sunli@outlook.com>
2018-11-28 15:44:16 +0800

    update

--- src/main/java/com/wsz/ecommerce/EcommerceApplication.java
+++ src/main/java/com/wsz/ecommerce/EcommerceApplication.java
@@ -1,9 +1,11 @@
 package com.wsz.ecommerce;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.wsz.ecommerce.mapper")
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/wsz/ecommerce/domain/User.java
@@ -0,0 +1,9 @@
+package com.wsz.ecommerce.domain;
+
+public class User {
+
+    private Integer userId;
+    private String userAccount;
+    private String userPassword;
+    private String 
+}
